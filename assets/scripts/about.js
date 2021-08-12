bdd3911a2e64f58d915ff9b5ab87f437ea9f73f6
George Kerridge <george@gkdsn.com>
2021-08-12 11:51:38 +0100

    Final About iteration Desktop

--- assets/scripts/about.js
+++ assets/scripts/about.js
@@ -4,6 +4,10 @@ let isDown = false;
 let startX;
 let scrollLeft;
 
+$(function() {
+    aboutLoad();
+});
+
 $aboutItems.addEventListener('mousedown', (e) => {
   isDown = true;
   startX = e.pageX - $aboutItems.offsetLeft;
@@ -26,11 +30,23 @@ $aboutItems.addEventListener('mousemove', (e) => {
 aboutClick = function(e) {
     let sectionName = $(this).attr('data-section'),
         $section = document.querySelector('#'+ sectionName),
-        sectionOffset = $section.offsetLeft;
+        sectionOffset = $section.offsetLeft,
+        sectionHref = $(this).attr('href');
     e.preventDefault();
     $aboutLink.removeClass('current')
     $(this).addClass('current')
     $aboutItems.scrollLeft = sectionOffset + 2;
+    window.history.replaceState(null, null, sectionHref);
+}
+aboutLoad = function(){
+    if (window.location.href.indexOf('?') != -1) {
+        let currentView = window.location.href.split('?')[1],
+        $section = document.querySelector('#'+ currentView),
+        sectionOffset = $section.offsetLeft;
+        $aboutLink.removeClass('current');
+        $('.js-about-link[data-section='+ currentView +']').addClass('current');
+        $aboutItems.scrollLeft = sectionOffset + 2;
+    }
 }
 
 $(document).on('click', '.js-about-link', aboutClick);
