6b44d2053661f97bfbf083c7f28a39d69c2f9576
George Kerridge <george@gkdsn.com>
2021-08-26 11:11:36 +0100

    Video test One

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -13,4 +13,15 @@ const swiper = new Swiper('.project-swiper-container', {
       nextEl: '.swiper-button.next',
       prevEl: '.swiper-button.prev',
     },
-  });
+});
+let $allVideos = $('.js-project-video');
+swiper.on('slideChange', function () {
+    $allVideos.each(function(){
+        let video = $(this).get(0);
+        console.log(video);
+        video.pause();
+    })
+    let $activeVideo = $('.swiper-slide-active .js-project-video').get(0);
+    $activeVideo.play();
+    console.log('slide changed');
+});
